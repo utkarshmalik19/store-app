d8fef9e1226de52b7493f33c8a22ef7c04285ef4
utkarshmalik19 <utkarshmalik19@gmail.com>
2023-12-14 22:23:27 +0530

    minor fixes

--- lib/presentation/screens/login_page.dart
+++ lib/presentation/screens/login_page.dart
@@ -15,101 +15,101 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
       backgroundColor: AppConstants.bgColor,
       body: Stack(children: [
-        Container(
-          child: Padding(
-            padding: const EdgeInsets.only(left: 35, top: 150),
-            child: Text(
-              "WELCOME\nTO STORE",
-              style: TextStyle(
-                  color: Colors.white, fontSize: 34, fontFamily: 'Inspiration'),
-            ),
+        Padding(
+          padding: const EdgeInsets.only(left: 35, top: 150),
+          child: Text(
+            "WELCOME\nTO STORE",
+            style: TextStyle(
+                color: Colors.white, fontSize: 34, fontFamily: 'Inspiration'),
           ),
         ),
         SingleChildScrollView(
-          child: Container(
-            child: Padding(
-              padding: EdgeInsets.only(
-                  top: MediaQuery.of(context).size.height * 0.4,
-                  right: 25,
-                  left: 25),
-              child: Column(
-                children: [
-                  TextField(
-                    decoration: InputDecoration(
-                        fillColor: Colors.grey.shade100,
-                        filled: true,
-                        hintText: "Email",
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10))),
+          child: Padding(
+            padding: EdgeInsets.only(
+                top: MediaQuery.of(context).size.height * 0.4,
+                right: 25,
+                left: 25),
+            child: Column(
+              children: [
+                TextField(
+                  decoration: InputDecoration(
+                      fillColor: Colors.grey.shade100,
+                      filled: true,
+                      hintText: "Email",
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10))),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      fillColor: Colors.grey.shade100,
+                      filled: true,
+                      hintText: "Password",
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10))),
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(9.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        "Sign In",
+                        style: TextStyle(
+                            fontSize: 27,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.white),
+                      ),
+                      CircleAvatar(
+                        radius: 20,
+                        backgroundColor: Colors.white,
+                        child: IconButton(
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          const DashboardPage()));
+                            },
+                            icon: const Icon(Icons.arrow_forward)),
+                      )
+                    ],
                   ),
-                  SizedBox(
-                    height: 30,
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(9.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Sign Up",
+                        style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                            decoration: TextDecoration.underline,
+                            color: Colors.white),
+                      ),
+                      Text(
+                        "Forgot Password?",
+                        style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                            decoration: TextDecoration.underline,
+                            color: Colors.white),
+                      ),
+                    ],
                   ),
-                  TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                        fillColor: Colors.grey.shade100,
-                        filled: true,
-                        hintText: "Password",
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10))),
-                  ),
-                  SizedBox(
-                    height: 50,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(9.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          "Sign In",
-                          style: TextStyle(
-                              fontSize: 27,
-                              fontWeight: FontWeight.w600,
-                              color: Colors.white),
-                        ),
-                        CircleAvatar(
-                          radius: 20,
-                          backgroundColor: Colors.white,
-                          child: IconButton(
-                              onPressed: () {
-                                Navigator.push(context, MaterialPageRoute(builder: (context) => DashboardPage()));
-                              },
-                              icon: Icon(Icons.arrow_forward)),
-                        )
-                      ],
-                    ),
-                  ),
-                  SizedBox(
-                    height: 40,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(9.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          "Sign Up",
-                          style: TextStyle(
-                              fontSize: 14,
-                              fontWeight: FontWeight.w400,
-                              decoration: TextDecoration.underline,
-                              color: Colors.white),
-                        ),
-                        Text(
-                          "Forgot Password?",
-                          style: TextStyle(
-                              fontSize: 14,
-                              fontWeight: FontWeight.w400,
-                              decoration: TextDecoration.underline,
-                              color: Colors.white),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         )
